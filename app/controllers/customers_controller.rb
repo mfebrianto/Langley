c106e3137f1dcc7d3f2693071706c58895b1125b
mfebrianto <michaelfebrianto@gmail.com>
2014-03-28 18:16:14 +1100

    create customer is done
    Signed-off-by: mfebrianto <michaelfebrianto@gmail.com>

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -3,6 +3,15 @@ class CustomersController < ApplicationController
   end
 
   def index
+    redis_url = "tcp://localhost:6379/0"  #todo: update to env
+    cia = CIA.new(redis_url)
+    customer_list = cia.list
+
+    customer_list.each do |customer|
+      puts "#{customer}"
+    end
+
+
   end
 
   def create
@@ -13,7 +22,11 @@ class CustomersController < ApplicationController
     @customer.yaml = params['yaml']
 
     #this code to send yaml
-    @customer.save
+    if @customer.save
+      @customer.publish
+    else
+      Rails.logger.info "todo"
+    end
 
     respond_to do |format|
         format.html { render action: 'index' }
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,12 +2,14 @@ class Customer
   attr_accessor :name, :yaml
 
   def save
-    Rails.logger.info ">>>>>#{name}>>>>#{yaml}"
-    publish
+    redis_url = "tcp://localhost:6379/0"  #todo: update to env
+    cia = CIA.new(redis_url)
+    cia.push(name, yaml)
   end
 
   def publish
-    Rails.logger.info ">>>>publish"
+    redis = Redis.new
+    redis.publish 'langley-publish', yaml
   end
 
 end
